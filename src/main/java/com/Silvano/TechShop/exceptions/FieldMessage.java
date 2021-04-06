ad63e6cfa602caa777c41c43b36f8c960b9fec3e
Silvano Charao Dias Junior <sjuniorfloripa666@gmail.com>
2021-04-06 08:39:42 -0300

    Personalizando retorno de validacao

---
+++ src/main/java/com/Silvano/TechShop/exceptions/FieldMessage.java
@@ -0,0 +1,16 @@
+package com.Silvano.TechShop.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class FieldMessage implements Serializable {
+
+    private String fieldName;
+    private String message;
+}
